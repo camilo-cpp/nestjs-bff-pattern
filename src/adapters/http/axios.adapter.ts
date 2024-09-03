27265f257be4edb885a10246c2d21c7e311ef68c
Juan Camilo <camil8cho@gmail.com>
2024-09-02 20:58:10 -0500

    feat: :sparkles: implement the adapter pattern for http clients

---
+++ src/adapters/http/axios.adapter.ts
@@ -0,0 +1,19 @@
+import { ConfigService } from '@nestjs/config';
+
+import axios from 'axios';
+
+import { Client } from 'src/graphql/types/types';
+import { HttpAdapterPort } from 'src/interfaces/http.port';
+
+export class AxiosAdapter implements HttpAdapterPort {
+  private readonly apiClientUrl: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.apiClientUrl = this.configService.get<string>('API_CLIENT');
+  }
+
+  async getClient(id: string): Promise<Client> {
+    const response = await axios.get(`${this.apiClientUrl}/client/${id}`);
+    return response.data;
+  }
+}
